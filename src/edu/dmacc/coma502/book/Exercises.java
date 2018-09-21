62ce0c5a612a345c577797006feaf086b0af1cd2
John Coffie <jjcoffie@dmacc.edu>
2018-09-20 23:09:48 -0500

    initial commit

---
+++ src/edu/dmacc/coma502/book/Exercises.java
@@ -0,0 +1,16 @@
+package edu.dmacc.coma502.book;
+
+import java.util.Scanner;
+
+public class Exercises {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("Please enter the number of bottles: ");
+		int bottles = in.nextInt();
+		System.out.println("Done");
+
+	}
+
+}
